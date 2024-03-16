fe7bc1fdff539f5b058c83b022fcfe7aa8f072fc
Gigiezi <giorgiezie2004@gmail.com>
2024-03-16 20:08:20 +0400

    fix: checked list item functional

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,8 +6,16 @@ const mainContent = document.getElementById('mainContentContainer');
 addButton.addEventListener('click', addItemFunction);
 
 function addItemFunction() {
-    nonListed.className = 'displayNone';
-    mainContent.innerHTML += '<div class="mainContentItem"><div class="itemContent"><img src="icons/check.svg" alt="check icon"><p>'+ mainInput.value +'</p></div><div><img onClick="this.parentElement.parentElement.remove()" src="icons/trash.svg" alt="trash Icon"></div></div>';
-    mainInput.value = '';
-}
+    let inputValued;
+    if(mainInput.value == ''){
+        inputValued = false;
+    }else{
+        inputValued = true;
+    }
+    if(inputValued){
+        nonListed.className = 'displayNone';
+        mainContent.innerHTML += '<div class="mainContentItem"><div class="itemContent"><img onClick="this.parentElement.parentElement.className += ` doneItem`" id="checkBox" src="icons/check.svg" alt="check icon"><p>'+ mainInput.value +'</p></div><div><img onClick="this.parentElement.parentElement.remove()" src="icons/trash.svg" alt="trash Icon"></div></div>';
+        mainInput.value = '';
+    }
+};
 
