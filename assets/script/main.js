cc66b2bef361cf58e3ea8b39f9df60d376b7a21b
Gigiezi <giorgiezie2004@gmail.com>
2024-03-16 22:20:18 +0400

    fix : button counter

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,10 +2,11 @@ const addButton = document.getElementById('mainButton');
 const mainInput = document.getElementById('mainInput');
 const nonListed = document.getElementById('noneListed')
 const mainContent = document.getElementById('mainContentContainer');
+const counter = document.getElementById('counter')
 
 addButton.addEventListener('click', addItemFunction);
-
-function addItemFunction() {
+let count = 0;
+function addItemFunction(event) {
     let inputValued;
     if(mainInput.value == ''){
         inputValued = false;
@@ -16,6 +17,8 @@ function addItemFunction() {
         nonListed.className = 'displayNone';
         mainContent.innerHTML += '<div class="mainContentItem"><div class="itemContent"><img onClick="this.parentElement.parentElement.className += ` doneItem`" id="checkBox" src="icons/check.svg" alt="check icon"><p>'+ mainInput.value +'</p></div><div><img onClick="this.parentElement.parentElement.remove()" src="icons/trash.svg" alt="trash Icon"></div></div>';
         mainInput.value = '';
+        count++;
+        counter.innerHTML = `${count}`;
     }
 };
 
